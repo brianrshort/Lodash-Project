fdfd937472df20649cdf79a354db67df98c76cde
Brian Short <brian.ralph.short@gmail.com>
2021-01-05 11:54:05 -0500

    Add pad method

--- _.js
+++ _.js
@@ -20,6 +20,39 @@ const _ = {
             return false; 
         }
     },
+    inRange: function(checkNum, startNum, endNum) {
+        if (!endNum) {
+            endNum = startNum;
+            startNum = 0;
+        }
+        if (startNum > endNum) {
+            let newStartVal = startNum;
+            let newEndVal = endNum;
+            startNum = newEndVal;
+            endNum = newStartVal; 
+        }
+        if (checkNum < endNum && checkNum >= startNum) {
+            return true;
+        } else {
+            return false; 
+        }
+    },
+    words: function(string) {
+        let arr = string.split(' ');
+        return arr;
+    },
+    pad: function(string, length) {
+        if (string.length > length) {
+            return string;
+        } else {
+            var dist = length - string.length; 
+            var front = Math.floor(dist / 2);
+            var back = Math.ceil(dist / 2);
+            var pad = " ";
+            var output = pad.repeat(front) + string + pad.repeat(back);
+            return output;
+        }
+    },
     
 }
 
