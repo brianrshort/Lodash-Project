84d56906d57e5fa48dc3817c9ca45775593e02f7
Brian Short <brian.ralph.short@gmail.com>
2021-01-05 11:53:02 -0500

    Add inRange method

--- _.js
+++ _.js
@@ -3,7 +3,24 @@ const _ = {
         let newNum = Math.max(numOne, numTwo);
         return Math.min(newNum, numThree);
     },
-
+    inRange: function(checkNum, startNum, endNum) {
+        if (!endNum) {
+            endNum = startNum;
+            startNum = 0;
+        }
+        if (startNum > endNum) {
+            let newStartVal = startNum;
+            let newEndVal = endNum;
+            startNum = newEndVal;
+            endNum = newStartVal; 
+        }
+        if (checkNum < endNum && checkNum >= startNum) {
+            return true;
+        } else {
+            return false; 
+        }
+    },
+    
 }
 
 
